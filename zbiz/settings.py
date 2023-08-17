f1e4ce9d25e759823adcaa48f69c2ad9683c6fe3
Paul Bailey <paul@neutron.studio>
2023-08-17 17:12:32 -0500

    app setup

--- zbiz/settings.py
+++ zbiz/settings.py
@@ -10,7 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import environ
+
 from pathlib import Path
+
+env = environ.Env(
+  DEBUG=(bool, False),
+  DATABASE_URL=(str, 'postgres://postgres:passyword@localhost:5432/zulipbiz')
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=+a7-(86#xbfi(l7i+n1rc_jvc%3&vvipxp%mb4heq%7csd!z5'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ntoggle',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +82,7 @@ WSGI_APPLICATION = 'zbiz.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db()
 }
 
 
